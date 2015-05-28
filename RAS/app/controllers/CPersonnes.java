a54b0002eaa3c381579a8ae84d3d38bcb893941e
Antonino D'Angelo <antonino.d-angelo@ensimag.grenoble-inp.fr>
2015-05-28 14:46:46 +0200

    template

--- RAS/app/controllers/CPersonnes.java
+++ RAS/app/controllers/CPersonnes.java
@@ -1,10 +1,25 @@
 package controllers;
 
+import java.util.List;
+
 import play.*;
 import play.mvc.*;
 import models.*;
 
-@CRUD.For(Personne.class)
-public class CPersonnes extends CRUD {
-
+public class CPersonnes extends Controller {
+	public static void test() {
+		try {
+			Personne une = new Personne("moi@test.fr", "Moi", "Moi", "0102030405");
+			Personne deux = new Personne("toi@test.fr", "Toi", "Toi", "0102030406");
+			
+			une.save();
+			deux.save();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		List<Personne> liste = Personne.findAll(); 
+		String title = "Ceci est un test";
+		
+		render("VPersonnes/liste_personnes.html", liste, title);
+	}
 }
