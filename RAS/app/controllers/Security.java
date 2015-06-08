5deb80418836a69c32276299bd8ff1cda2f29b27
perrettc <perrette.c@gmail.com>
2015-06-08 16:42:06 +0200

    authentification + deconnection +cookies

---
+++ RAS/app/controllers/Security.java
@@ -0,0 +1,18 @@
+package controllers;
+ 
+import models.*;
+ 
+public class Security extends Secure.Security {
+	
+    static boolean authenticate(String username, String password) {
+    	Personne user = Personne.find("byEmail",username).first();
+    	
+    	if(user == null)
+    	{
+    		return false;
+    	}else{
+    		return user.verifMotDePasse(password);
+    	}
+    }
+    
+}
