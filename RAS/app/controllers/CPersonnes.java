bafe358b4f4ce8525d87c68f8dde8492a3a34ada
Antonino D'Angelo <antonino.d-angelo@ensimag.grenoble-inp.fr>
2015-06-10 22:24:59 +0200

    commit

--- RAS/app/controllers/CPersonnes.java
+++ RAS/app/controllers/CPersonnes.java
@@ -26,7 +26,7 @@ public class CPersonnes extends Controller {
 		render("VPersonnes/liste_personnes.html", liste, title);
 	}
 	
-	public static void creerUtilisateur(String nom, String prenom, String email, String telephone, String adresse, String codePostal, String ville) {
+	public static void creerUtilisateur(String nom, String prenom, String email, String telephone, String adresse, String codePostal, String ville)  {
 		validation.required(nom);
 		validation.required(prenom);
 		validation.required(email);
